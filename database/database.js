ed8ae6c5e27970fb1df1aeb5d09a6ae25255a69a
ahmadharthey <ahmoood@outlook.com>
2023-11-26 14:26:22 +0300

    code working as intended now

--- database/database.js
+++ database/database.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
-var db=async()=>{
-    await mongoose.connect ("mongodb+srv://sha050:shakthi050@first.v7omroo.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("yesssss")}).catch((err)=>{console.log(err)});
+var db = async () => {
+    await mongoose.connect("mongodb+srv://sha050:shakthi050@first.v7omroo.mongodb.net/?retryWrites=true&w=majority").then(() => { console.log("yesssss") }).catch((err) => { console.log(err) });
     console.log("work");
 }
 
 db();
 
-const login=new mongoose.Schema({
-    username:String,
-    password:String,
-    list:mongoose.Schema.Types.Mixed
+const login = new mongoose.Schema({
+    username: String,
+    password: String,
+    list: mongoose.Schema.Types.Array
 })
 
 
-const log_var=new mongoose.model("owntest",login);
+const log_var = new mongoose.model("owntest", login);
 
-module.exports={log_var};
+module.exports = { log_var };
--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-const express=require('express');
-const db=require('./database/database.js');
-const app=express();
-let name="";
-app.set("view engine","ejs");
-app.use(express.json(),express.urlencoded({extended:true}));
-app.use('/javascript',express.static(__dirname+'/javascript')) 
-app.listen(3000,()=>{
+const express = require('express');
+const db = require('./database/database.js');
+const app = express();
+let name = "";
+app.set("view engine", "ejs");
+app.use(express.json(), express.urlencoded({ extended: true }));
+app.use('/javascript', express.static(__dirname + '/javascript'))
+app.listen(3000, () => {
     console.log("started");
 })
 
-app.get('/',(req,res)=>{
-    res.render(__dirname+"/pages/index.ejs");
+app.get('/', (req, res) => {
+    res.render(__dirname + "/pages/index.ejs");
 })
-app.post('/sendata',(req,res)=>{
-    const data=req.body;
-    name=data.user; 
-    db.log_var.insertMany({username:data.user,password:data.pass,list:['']});
+app.post('/sendata', (req, res) => {
+    const data = req.body;
+    name = data.user;
+    db.log_var.insertMany({ username: data.user, password: data.pass });
     res.send('Sucess');
 })
 
-app.get('/find',(req,res)=>{
-    res.render(__dirname+"/pages/check.ejs");
+app.get('/find', (req, res) => {
+    res.render(__dirname + "/pages/check.ejs");
 })
 
-app.post('/details',(req,res)=>{
-    const data=req.body
-    db.log_var.findOneAndUpdate({username:name},{$set:{list:data.list}})
+app.post('/details', (req, res) => {
+    const data = req.body
+    db.log_var.findOneAndUpdate({ username: name }, { $set: { list: data.list } }, { new: true }).then((doc) => {
+        console.log(doc)
+    })
 })
 
-app.get('/list',(req,res)=>{
-    db.log_var.findOne({username:name}).then((doc)=>{
-        console.log(name);
+app.get('/list', (req, res) => {
+    db.log_var.findOne({ username: name }).then((doc) => {
         res.json(doc);
     })
 })
 
-app.get('/fuckingTodo',(req,res)=>{
-    res.sendFile(__dirname+'/pages/todo.html')
+app.get('/fuckingTodo', (req, res) => {
+    res.sendFile(__dirname + '/pages/todo.html')
 })
